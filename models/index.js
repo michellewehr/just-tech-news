c91003b67918c22091df1601f532539292dc4589
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-11 22:29:55 -0400

    added the post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-//but for now it'll just be for collecting and exporting the User model data
-const User = require('./User');
+const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
