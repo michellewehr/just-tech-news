f4ab0ab36382a239a7247e01d418707841246593
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-18 11:00:13 -0400

    completed homepage with handlebars

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
 //stylesheet 
 const path = require('path');
+const express = require('express');
+const routes = require('./controllers/');
+//handlebars
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.use(routes);
+
+
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
 //stylesheet 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 // turn on connection to db and server
 //In the sync method, there is a configuration parameter { force: false }. If we change the value of the force property to true, then the database connection must sync with the model definitions and associations. By forcing the sync method to true, we will make the tables re-create if there are any association changes.
